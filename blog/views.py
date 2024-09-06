2d129e28cd1bbad8438f20aa6f4b00f5b780d903
seudsahm2 <seudsahm1@gmail.com>
2024-09-06 15:19:55 +0300

    urls are converted in to cannonical and SEO optimized url

--- blog/views.py
+++ blog/views.py
@@ -9,15 +9,18 @@ def post_list(request):
     return render(request, 'blog/post/list.html',{'posts':posts})
 
 
-def post_detail(request, id):
+def post_detail(request, year, month, day, post):
     # try:
     #     post = Post.published.get(id = id)
     # except Post.DoesNotExist:
     #     raise Http404("No Post Found")
     post = get_object_or_404(
         Post,
-        id = id,
-        status = Post.Status.PUBLISHED
+        status = Post.Status.PUBLISHED,
+        slug = post,
+        publish__year = year,
+        publish__month = month,
+        publish__day = day
     )
     return render(
         request, 
