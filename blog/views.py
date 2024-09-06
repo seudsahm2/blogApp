5d6041d1868f505ecccd077fee7179c865cfe08d
seudsahm2 <seudsahm1@gmail.com>
2024-09-06 19:25:29 +0300

    comment implemented

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,13 @@
 from django.core.paginator import EmptyPage,PageNotAnInteger, Paginator
 from django.core.mail import send_mail
+
 from django.shortcuts import render, get_object_or_404
+
 from django.views.generic import ListView
+from django.views.decorators.http import require_POST
 
 from .models import Post
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 
 # from django.http import Http404
 # Create your views here.
@@ -46,10 +49,19 @@ def post_detail(request, year, month, day, post):
         publish__month = month,
         publish__day = day
     )
+
+    comments = post.comments.filter(active=True)
+
+    form = CommentForm()
+
     return render(
         request, 
         'blog/post/detail.html',
-        {'post':post}
+        {
+            'post': post,
+            'comments': comments,
+            'form': form
+        }
     )
 
 
@@ -98,3 +110,31 @@ def post_share(request, post_id):
             'sent': sent
          }
     )  
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(
+        Post,
+        id = post_id,
+        status = Post.Status.PUBLISHED
+    )
+
+    comment = None
+
+    form = CommentForm(request.POST)
+    if form.is_valid():
+        comment = form.save(commit = False)
+        comment.post = post
+        comment.save()
+
+    return render(
+        request,
+        'blog/post/comment.html',
+        {
+            'post':post,
+            'form':form,
+            'comment':comment
+        }
+        
+    )
