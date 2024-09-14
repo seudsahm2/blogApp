a0f45d36b71027ef9fa300f51d0f94b766cf83d1
seudsahm2 <seudsahm1@gmail.com>
2024-09-14 21:07:50 +0300

    weigthing queries implemeted and blog finished

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.core.paginator import EmptyPage,PageNotAnInteger, Paginator
 from django.core.mail import send_mail
 
-from django.contrib.postgres.search import SearchVector, SearchRank, SearchQuery
+from django.contrib.postgres.search import SearchVector, SearchRank, SearchQuery, TrigramSimilarity
 
 from django.db.models import Count
 
@@ -177,14 +177,22 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            search_vector = SearchVector('title','body')
-            search_query = SearchQuery(query)
+            # search_vector = SearchVector('title', weight='A')+ SearchVector('body', weight = 'B')
+            # search_query = SearchQuery(query)
+            # results = (
+            #     Post.published.annotate(
+            #         search = search_vector,
+            #         rank = SearchRank(search_vector,search_query)
+            #     ).filter(rank__gte=0.3)
+            #     .order_by('-rank')
+            # )
+
+            # the bove is commented to use the following trigram similarity search
             results = (
                 Post.published.annotate(
-                    search = search_vector,
-                    rank = SearchRank(search_vector,search_query)
-                ).filter(search = search_query)
-                .order_by('-rank')
+                    similarity = TrigramSimilarity('title',query)
+                ).filter(similarity__gt=0.1)
+                .order_by('-similarity')
             )
     return render(
         request,
